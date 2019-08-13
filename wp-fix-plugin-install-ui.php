d7450d5dc7a96ba55e244af038824c9445718f0f
Fahad Murtaza <info@fahdmurtaza.com>
2019-08-13 18:28:07 +0400

    Create wp-fix-plugin-install-ui.php

---
+++ wp-fix-plugin-install-ui.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: Clean the Plugin Install screen
+ * Plugin URI: https://gist.github.com/fahdi/bd38ec99927469145168df1196a98363
+ * Description: Clean the CSS which fixes the plugin info display issues in wordpress admin
+ * Version: 1.0
+ * Author: Fahad Murtaza
+ * Author URI: https://fahdmurtaza.com
+ */
+ 
+add_action('admin_head', 'fm_clean_plugin_info_screen');
+
+function fm_clean_plugin_info_screen() {
+  echo '<style>
+    @media screen and (max-width: 782px){ 
+	.wrap {
+    	clear: none !important;    
+	}
+  </style>';
+}
